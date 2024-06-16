e725f5eff6481865a8847b33f45b35be0af1456f
sdevich <sdevich@wgu.edu>
2024-06-16 12:35:58 -0700

    Deleted DeletePartValidator.java and ValidDeletePart.java based on a code inspection and both files showing as unused.

--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -1,6 +1,5 @@
 package com.example.demo.domain;
 
-import com.example.demo.validators.ValidDeletePart;
 import com.example.demo.validators.ValidMinInv;
 import com.example.demo.validators.ValidMaxInv;
 
@@ -18,7 +17,6 @@ import java.util.Set;
  *
  */
 @Entity
-@ValidDeletePart
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="part_type",discriminatorType = DiscriminatorType.INTEGER)
 @Table(name="Parts")
